dcec775f423d592922c2f8194aab290500703e99
Foxxai <546f6464.jbb@gmail.com>
2025-09-14 08:58:49 -0400

    feat: Add VS Code tasks and environment management for site and photo processing

---
+++ scripts/process_photos.py
@@ -0,0 +1,8 @@
+# Photo processing script placeholder
+# This will be implemented when we work on the gallery feature
+
+def process_photos():
+    print("Photo processing will be implemented here")
+
+if __name__ == "__main__":
+    process_photos()
